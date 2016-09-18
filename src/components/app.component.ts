50d9cf9392a46083faf60889f6795ea596008938
KadoTomohiro <corner0427@gmail.com>
2016-09-18 18:15:40 +0900

    create component

---
+++ src/components/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+
+  template: `
+  <h1>{{title}}</h1>
+  `,
+})
+export class AppComponent {
+  title:string = 'Hello World!';
+}
