82c6f7eee31b0b4275e47e0aaecd9d2298b0f8c2
KadoTomohiro <corner0427@gmail.com>
2016-09-18 18:11:05 +0900

    create module

---
+++ src/modules/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule }       from '@angular/core';
+
+import { AppComponent }   from '../components/app.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [
+    AppComponent,
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+}
